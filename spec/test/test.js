a46986773195b8108ae5668d117613436f7bb73f
Miles MJ Jamon <31880601+pvzzombs@users.noreply.github.com>
2020-11-13 18:00:54 +0800

    Added test and package.json
    * test using jasmine

---
+++ spec/test/test.js
@@ -0,0 +1,44 @@
+var percent = 0;
+describe("The arb.js test suite", function(){
+  var arb = require("../../dist/arb.js");
+  
+  var mulTestPair = ["1234567890", "9876543210"];
+  var mulTestAnswer = "12193263111263526900.0";
+  var addTestPair = ["1234567890", "9876543210"];
+  var addTestAnswer = "11111111100.0";
+  var subTestPair = ["1234567890", "9876543210"];
+  var subTestAnswer = "-8641975320.0";
+  var divTestPair = ["1234567890", "9876543210"];
+  var divTestAnswer = "0.1249999988609375000";
+  
+  
+  describe("Addition tests", function(){
+    it("should work properly on addition", function(){
+      //console.info("Running Addition Test...");
+      expect(arb(addTestPair[0]).add(addTestPair[1]).value).toBe(addTestAnswer);
+    });
+  });
+  
+  describe("Subtraction tests", function(){
+    //console.info("Running Subtraction Test...");
+    it("should work properly on subtraction", function(){
+      expect(arb(subTestPair[0]).sub(subTestPair[1]).value).toBe(subTestAnswer);
+    });
+  });
+  
+  describe("Multiplication tests", function(){
+    //console.info("Running multiplication Test...");
+    it("should work properly on multiplication", function(){
+      expect(arb(mulTestPair[0]).mul(mulTestPair[1]).value).toBe(mulTestAnswer);
+    });
+  });
+  
+  describe("Division tests", function(){
+    //console.info("Running Division Test...");
+    it("should work properly on division", function(){
+      expect(arb(divTestPair[0]).div(divTestPair[1]).value.substr(0, 21)).toBe(divTestAnswer);
+      expect(arb("1").div("0.0001").value).toBe("10000.0");
+      expect(arb("1").div("89").value.substr(0, 22)).toBe("0.01123595505617977528");
+    });
+  });
+});
